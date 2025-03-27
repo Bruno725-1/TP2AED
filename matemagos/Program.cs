5359f29936f2b35cde1523ae1083b5ba96c85e9a
Bruno César Lima Araújo <1561719@sga.pucminas.br>
2025-03-26 22:30:14 -0300

    Meu primeiro commit

---
+++ matemagos/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Retirar o prompt antes de mandar para o Verde");
+        int n = int.Parse(Console.ReadLine());
+        int num = 0;
+        int i = 0;
+        if (i < n) {
+            num = int.Parse(Console.ReadLine());
+            Console.WriteLine(ConverterNumero(num));
+            i++;
+        }
+        Console.WriteLine(num);
+    }
+    static int ConverterNumero (int num) {
+        int binario = num;
+        if (binario == 1) {
+            return 1;
+        }
+        else {
+            int resto = binario % 2;
+            Console.Write(resto);
+            return ConverterNumero(binario / 2);
+        }
+    }
+}
