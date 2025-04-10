32fa56121a2977299e6f128bfa9dc110de1a1d2f
Bruno César Lima Araújo <1561719@sga.pucminas.br>
2025-04-09 21:54:11 -0300

    Mensagem

---
+++ bancos/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Olá, mundo!");
+        string entrada = "";
+        do {
+            entrada = Console.ReadLine();
+        } while (entrada != "-1");
+    }
+}
